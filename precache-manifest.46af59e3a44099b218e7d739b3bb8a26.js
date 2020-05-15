325d96a3d708bbcc759e3fa73595387959d56b64
hayden cordeiro <cordeirohayden@gmail.com>
2020-05-15 06:25:54 -0700

    Updates

---
+++ precache-manifest.46af59e3a44099b218e7d739b3bb8a26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb732bdd7016f6e5a4b2b7c9f325387c",
+    "url": "/DoggyReact/index.html"
+  },
+  {
+    "revision": "a08707ab1e6a47a9a217",
+    "url": "/DoggyReact/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "90e127b3cbc087c84a70",
+    "url": "/DoggyReact/static/css/main.3376d688.chunk.css"
+  },
+  {
+    "revision": "a08707ab1e6a47a9a217",
+    "url": "/DoggyReact/static/js/2.fdadf157.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DoggyReact/static/js/2.fdadf157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90e127b3cbc087c84a70",
+    "url": "/DoggyReact/static/js/main.0b1610d1.chunk.js"
+  },
+  {
+    "revision": "8c0f3b457244607db53e",
+    "url": "/DoggyReact/static/js/runtime-main.a9554ffa.js"
+  }
+]);
