ed60a81c3fd8b7b0c091fc562ca825ebfe9c2c09
hayden cordeiro <cordeirohayden@gmail.com>
2020-05-15 06:11:59 -0700

    Doggies v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
+
+const [imageURL,setImageURL]=useState('')
+const[changeImage,setChangeImage]=useState(0)
+const[prevImage,setPrevImage]=useState('')
+useEffect(
+  ()=>{
+    getImage()
+  },[changeImage]
+)
+  const URL='https://dog.ceo/api/breeds/image/random';
+  
+
+  const getImage=async()=>{
+    const respose=await fetch(URL);
+    const data=await respose.json();
+    console.log(data.message);
+    setImageURL(data.message);
+    if(changeImage===0){
+      setPrevImage(imageURL);
+    }
+
+    }
+
+
+const nextImage=()=>
+{
+  setPrevImage(imageURL);
+  setChangeImage(changeImage+1);
+  
+}
+
+const PrevImage=()=>
+{
+  
+  setImageURL(prevImage);
+  
+}
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div align='center'>
+<div class="text-center">
+  <img src={imageURL}   className="rounded m-2" alt="..." />
+</div>
+<button onClick={PrevImage} className='btn btn-danger m-2'>Prev Image</button>
+  <button onClick={nextImage} className='btn btn-primary m-2'>Generate Random</button>
+  
+</div>
   );
 }
 
